4ce5f7c69b1ecc339c3735913cf8ff57c3e2ddaa
Christopher Eckardt <christopher.eckardt@visual-meta.com>
2019-03-27 14:44:34 +0100

    Updates

---
+++ precache-manifest.6cecc68bbb8e4a99f2bc796f6e10707c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2f18660d2c17cdb25be",
+    "url": "/modernfetchingimages/static/js/runtime~main.c05ed79c.js"
+  },
+  {
+    "revision": "2458d90ce839bdda6bb8",
+    "url": "/modernfetchingimages/static/js/main.704502bb.chunk.js"
+  },
+  {
+    "revision": "e0794957992fadb24acb",
+    "url": "/modernfetchingimages/static/js/2.03c2f0c1.chunk.js"
+  },
+  {
+    "revision": "2458d90ce839bdda6bb8",
+    "url": "/modernfetchingimages/static/css/main.841ac6f7.chunk.css"
+  },
+  {
+    "revision": "215e50afae658818efb7378c9c8501ea",
+    "url": "/modernfetchingimages/index.html"
+  }
+];
